8f6319bfac007e6f2b9cd6801f95ea47e3a7f033
AlfredPichard <alfred.pichard@free.fr>
2022-01-17 18:21:15 +0100

    improvements

--- main.py
+++ main.py
@@ -1,41 +1,58 @@
 import utils
-from sclib import SoundcloudAPI
+from sclib.sync import SoundcloudAPI
 
-def main(): 
+
+def main():
     config = utils.get_config()
     sc_api = SoundcloudAPI()
-    playlist_ids = utils.prompt_to_fill(config['playlists'].keys(), 'Select playlist to download: ')
+    playlist_ids = utils.prompt_to_fill(
+        config["playlists"].keys(), "Select playlist to download: "
+    )
 
-    already_dl_tracks = set()
+    already_dl_tracks = set(
+        open(config["paths"]["DL_PATH"] + "/already_downloaded.txt").read().splitlines()
+    )
     tracks_to_dl = set()
     dropped_tracks = set()
+    downloaded_tracks = set()
 
     for playlist_id in playlist_ids:
-        playlist_content = sc_api.resolve(config['playlists'][playlist_id])
-        
+        playlist_content = sc_api.resolve(config["playlists"][playlist_id])
+
         for track in playlist_content.tracks:
             if track.id not in already_dl_tracks:
                 tracks_to_dl.add(track)
-                already_dl_tracks.add(track.id)
 
-        print(f"Found {len(playlist_content)} tracks in {playlist_content.title}, including {len(tracks_to_dl)} new tracks.")
+        print(
+            f"Found {len(playlist_content)} tracks in {playlist_content.title}, including {len(tracks_to_dl)} new tracks."
+        )
         if not utils.confirm():
             continue
 
         print(f"Downloading tracks...")
         for track in tracks_to_dl:
             try:
-                utils.download(track)
+                utils.download(track, config["paths"]["DL_PATH"], playlist_id)
                 print(f"Downloaded {track.title}")
+                downloaded_tracks.add(track.id)
             except Exception as e:
                 print(f"Could not download {track.title}")
+                print(e)
                 dropped_tracks.add(track)
 
-        print(f"Finished downloading {len(tracks_to_dl) - len(dropped_tracks)} tracks.")
-        print(f"Could not download {len(dropped_tracks)} tracks.")
+        print(f"Finished downloading {len(downloaded_tracks)} tracks.")
+        if len(dropped_tracks) > 0:
+            print(f"Could not download {len(dropped_tracks)} tracks.")
+        print("Updating history file...")
+
+        with open(config["paths"]["DL_PATH"] + "/already_downloaded.txt", "a") as f:
+            for downloaded_track in downloaded_tracks:
+                f.write(downloaded_track + "\n")
+
+        print("Done !")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
--- utils.py
+++ utils.py
@@ -6,22 +6,46 @@ import configparser
 
 def get_config():
     config = configparser.ConfigParser()
-    config.read('settings.ini')
+    config.read("settings.ini")
     return config
 
 
 def prompt_to_fill(options, prompt_text):
-    completer = prompt_toolkit.completion.WordCompleter(options, ignore_case=True, pattern=re.compile(r'\S*'))
+    completer = prompt_toolkit.completion.WordCompleter(
+        options, ignore_case=True, pattern=re.compile(r"\S*")
+    )
     response = prompt_toolkit.prompt(prompt_text, completer=completer)
-    return {target for response_slice in response.split(',') for target in response_slice.split(' ')}
+    return {
+        target
+        for response_slice in response.split(",")
+        for target in response_slice.split(" ")
+    }
 
 
 def confirm():
-    return input("Download (Y/n) ?").lower() in ['y', '']
+    return input("Download (Y/n) ?").lower() in ["y", ""]
 
 
-def download(track):
+def de_emoji(text):
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "]+",
+        flags=re.UNICODE,
+    )
+    return emoji_pattern.sub(r"", text)  # no emoji
+
+
+def download(track, dl_path, playlist):
     mp3_stream_file = urlopen(track.get_stream_url())
-    with open(f"{track.title}.mp3", "wb") as mp3_file:
+    with open(
+        dl_path
+        + "/"
+        + playlist
+        + f"/{de_emoji(track.artist)} - {de_emoji(track.title)}.mp3",
+        "wb",
+    ) as mp3_file:
         mp3_file.write(mp3_stream_file.read())
-    
